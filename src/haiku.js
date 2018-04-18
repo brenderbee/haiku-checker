d2be7ccbacb9035490b8486fc96c5f78f63d36dc
Brenda Franco and julia sheremet <brenda@>
2018-04-18 16:20:31 -0700

    functions based on all the specs in the program

--- src/haiku.js
+++ src/haiku.js
@@ -23,7 +23,7 @@ export class Haiku {
     let letters = [];
     letters = inputWord.split("");
     let count = 0;
-    for (let i =0; i < letters.length; i++) {
+    for (let i =1; i < letters.length; i++) {
       if (letters[i].match(/[aeiouAEIOU]/) && letters[i-1].match(/[aeiouAEIOU]/)) {
         count -= 1;
       }
