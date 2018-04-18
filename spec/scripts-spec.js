9e5d440ce28921e8bfc748912372eed77200220e
Brenda Franco and julia sheremet <brenda@>
2018-04-18 11:24:17 -0700

    behave based on spec two

--- spec/scripts-spec.js
+++ spec/scripts-spec.js
@@ -9,4 +9,16 @@ describe('Haiku', function() {
     expect(haikuTest.input3).toEqual("word3");
   });
 
+  it('should return number of vowels in a single word', function() {
+    let haikuTest = new Haiku("hello", "word2", "word3");
+    let output = haikuTest.countVowels(haikuTest.input1);
+    expect(output).toEqual(2);
+  });
+
+  // it('test if split is working', function() {
+  //   let haikuTest = new Haiku("word", "word2", "word3");
+  //   let output = haikuTest.countVowels(haikuTest.input1);
+  //   expect(output).toEqual(["w","o","r","d"]);
+  // });
+
 });
--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,17 @@ export class Haiku {
     this.input2 = input2;
     this.input3 = input3;
   }
+  countVowels(inputWord) {
+    let letters = [];
+    letters = inputWord.split("");
+    let count = 0;
+    letters.forEach(function(letter){
+      if(letter.match(/[aeiouAEIOU]/)) {
+        count += 1;
+      }
+    });
+    return count;
+  }
 }
 
 // let haikuTest = new Haiku("word1", "word2", "word3");
